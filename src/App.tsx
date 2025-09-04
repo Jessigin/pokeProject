f695fbb6b5923bebf8b35197b143e507f3dbd1eb
AdánHF <35301303+AdanHdzF@users.noreply.github.com>
2025-09-04 11:40:09 -0600

    Refactor PokemonListPage to use useMemo for optimized data fetching and rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,10 @@ import './App.css';
 import { Suspense, lazy } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-// import PokemonCard from './components/PokemonCard';
-// import PokemonList from './components/PokemonList';
-// import type { Pokemon } from './features/properties/model/types';
-// import { PokemonApi } from './services/pokemonApi';
-
 const PokemonPage = lazy(() => import('./pages/PokemonPage'));
 const PokemonListPage = lazy(() => import('./pages/PokemonListPage'));
 
 function App() {
-	// const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-	// const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		const dataPokemonList = await PokemonApi.getPokemonList();
-
-	// 		const dataPokemon = await PokemonApi.searchPokemon('pikachu');
-
-	// 		// const data = await PokemonApi.getPokemonSpecies(25);
-
-	// 		setPokemon(dataPokemon);
-	// 		setPokemonList(dataPokemonList);
-	// 	};
-
-	// 	fetchData();
-	// }, []);
-
 	return (
 		<Router>
 			<Suspense fallback={<div>Loading...</div>}>
--- src/pages/PokemonListPage.tsx
+++ src/pages/PokemonListPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import PokemonList from '../components/PokemonList';
 import type { Pokemon } from '../features/properties/model/types';
@@ -7,21 +7,20 @@ import { PokemonApi } from '../services/pokemonApi';
 export default function PokemonListPage() {
 	const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
-	useEffect(() => {
+	const pokemonListMemo = useMemo(() => {
 		const fetchData = async () => {
 			const dataPokemonList = await PokemonApi.getPokemonList();
-
 			setPokemonList(dataPokemonList);
 		};
-
 		fetchData();
-	}, []);
+		return pokemonList;
+	}, [pokemonList]);
 
 	return (
 		<div>
 			<h1>Pokemon List Page</h1>
 
-			<PokemonList pokemonList={pokemonList} />
+			<PokemonList pokemonList={pokemonListMemo} />
 		</div>
 	);
 }
