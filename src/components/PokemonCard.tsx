adc29dc5bb20e8dae0cb01ba25e5fc3a409e72b8
AdánHF <35301303+AdanHdzF@users.noreply.github.com>
2025-09-04 11:32:40 -0600

    Refactor PokemonCard and PokemonPage components for improved rendering and data handling

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { type Pokemon } from '../features/properties/model/types';
 
 export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
@@ -7,8 +8,19 @@ export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
 
 	return (
 		<div>
-			<div>PokemonCard</div>
-			<h2>{pokemon.name}</h2>
+			{pokemon ? (
+				<div>
+					<h2>{pokemon.name}</h2>
+					<img
+						src={pokemon.sprites.front_default ?? undefined}
+						alt={pokemon.name}
+					/>
+					<p>Height: {pokemon.height}</p>
+					<Link to="/">Back to Home</Link>
+				</div>
+			) : (
+				<p>Loading...</p>
+			)}
 		</div>
 	);
 }
--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PokemonCard from '../components/PokemonCard';
+import type { Pokemon } from '../features/properties/model/types';
+import { PokemonApi } from '../services/pokemonApi';
+
 export default function PokemonPage() {
+	const { name } = useParams<{ name: string }>();
+
+	const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const dataPokemonList = await PokemonApi.searchPokemon(
+				name as string
+			);
+
+			setPokemon(dataPokemonList);
+		};
+
+		fetchData();
+	}, [name]);
+
 	return (
 		<div>
-			<h1>Pokemon Page</h1>
+			{pokemon ? <PokemonCard pokemon={pokemon} /> : <p>Loading...</p>}
 		</div>
 	);
 }
