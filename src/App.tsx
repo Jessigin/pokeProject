06c72d3676ce21d12e0322022266af5459423183
AdánHF <35301303+AdanHdzF@users.noreply.github.com>
2025-09-04 11:22:18 -0600

    Refactor App component to use lazy loading for Pokemon pages and remove unused state and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 
-import PokemonCard from './components/PokemonCard';
-import PokemonList from './components/PokemonList';
-import type { Pokemon } from './features/properties/model/types';
-import { PokemonApi } from './services/pokemonApi';
+import { Suspense, lazy } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+// import PokemonCard from './components/PokemonCard';
+// import PokemonList from './components/PokemonList';
+// import type { Pokemon } from './features/properties/model/types';
+// import { PokemonApi } from './services/pokemonApi';
+
+const PokemonPage = lazy(() => import('./pages/PokemonPage'));
+const PokemonListPage = lazy(() => import('./pages/PokemonListPage'));
 
 function App() {
-	const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-	const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+	// const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+	// const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const dataPokemonList = await PokemonApi.getPokemonList();
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		const dataPokemonList = await PokemonApi.getPokemonList();
 
-			const dataPokemon = await PokemonApi.searchPokemon('pikachu');
+	// 		const dataPokemon = await PokemonApi.searchPokemon('pikachu');
 
-			// const data = await PokemonApi.getPokemonSpecies(25);
+	// 		// const data = await PokemonApi.getPokemonSpecies(25);
 
-			setPokemon(dataPokemon);
-			setPokemonList(dataPokemonList);
-		};
+	// 		setPokemon(dataPokemon);
+	// 		setPokemonList(dataPokemonList);
+	// 	};
 
-		fetchData();
-	}, []);
+	// 	fetchData();
+	// }, []);
 
 	return (
-		<>
-			<PokemonCard pokemon={pokemon} />
-			<PokemonList pokemonList={pokemonList} />
-		</>
+		<Router>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path="/" element={<PokemonListPage />} />
+					<Route path="/pokemon/:name" element={<PokemonPage />} />
+
+					<Route path="*" element={<PokemonListPage />} />
+				</Routes>
+			</Suspense>
+		</Router>
 	);
 }
 
