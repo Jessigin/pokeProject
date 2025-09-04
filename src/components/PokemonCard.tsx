5773c4b3ea357acc965717e0c6ceba7fff564f5a
Jessica Rocha <esjessi17@gmail.com>
2025-09-04 12:22:11 -0600

    JessiG
    Se agregaron estilos al pokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,40 @@
 import { Link } from 'react-router-dom';
 import { type Pokemon } from '../features/properties/model/types';
+import './PokemonCard.css'; // Import CSS file
 
 export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
-	console.log(pokemon);
+  console.log(pokemon);
 
-	if (!pokemon) return <div>No Pokemon found</div>;
+  if (!pokemon) return <div className="pokemon-card">No Pokemon found</div>;
 
-	return (
-		<div>
-			{pokemon ? (
-				<div>
-					<h2>{pokemon.name}</h2>
-					<img
-						src={pokemon.sprites.front_default ?? undefined}
-						alt={pokemon.name}
-					/>
-					<p>Height: {pokemon.height}</p>
-					<Link to="/">Back to Home</Link>
-				</div>
-			) : (
-				<p>Loading...</p>
-			)}
-		</div>
-	);
+  return (
+    <div className="pokemon-card">
+      {pokemon ? (
+        <div className="pokemon-card-content">
+          <h2 className="pokemon-card-title">{pokemon.name}</h2>
+          <img
+            className="pokemon-card-image"
+            src={pokemon.sprites.front_default ?? undefined}
+            alt={pokemon.name}
+          />
+          <p>ID: {pokemon.id}</p>
+          <p>Insignias:</p>
+          <p>Altura: {pokemon.height}</p>
+          <p>Experiencia: {pokemon.base_experience}</p>
+          <p>
+            Habilidades:{' '}
+            {pokemon.abilities
+              .map((ability) => ability.ability.name)
+              .join(', ')}
+          </p>
+
+          <Link className="pokemon-card-link" to="/">
+            Back to Home
+          </Link>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
