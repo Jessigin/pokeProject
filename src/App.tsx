bc8703f809946f441ccb6fd4ad6d8ef3a8ebe140
AdánHF <35301303+AdanHdzF@users.noreply.github.com>
2025-09-04 12:10:59 -0600

    Replace loading fallback in Suspense with LoadingSpinner component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import { Suspense, lazy } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
+import LoadingSpinner from './components/LoadingSpinner';
+
 const PokemonPage = lazy(() => import('./pages/PokemonPage'));
 const PokemonListPage = lazy(() => import('./pages/PokemonListPage'));
 
 function App() {
 	return (
 		<Router>
-			<Suspense fallback={<div>Loading...</div>}>
+			<Suspense fallback={<LoadingSpinner />}>
 				<Routes>
 					<Route path="/" element={<PokemonListPage />} />
 					<Route path="/pokemon/:name" element={<PokemonPage />} />
